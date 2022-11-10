64da985445f8f1346760b49fbe5ac8a9cd4f8013
Ty Andrews <ty.elgin.andrews@gmail.com>
2022-11-10 10:21:20 -0800

    Setup repo from cookiecutter-datascience template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A Twitter bot that uses Markov models to generate new tweets based on different corpuses.',
+    author='Ty Andrews',
+    license='MIT',
+)
